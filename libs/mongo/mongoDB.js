50c828d2b3fc4fb6a32af8a3f8ec83752a43e2f4
= <=>
2024-01-28 01:46:29 +0530

    api bug fix

--- libs/mongo/mongoDB.js
+++ libs/mongo/mongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectTomongoDB=async()=>{
    try {
-    await mongoose.connect(process.env.MONGODBURL)
+    await mongoose.connect('mongodb+srv://dipakhade214:TwF7ZImbgD5J5zkt@cluster0.saoibto.mongodb.net/portfolioconnect')
    } catch (error) {
     console.log(error)
    }
